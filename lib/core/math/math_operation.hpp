90744ab8abf7f085bba8193b400eafbff3fc2867
AsphodelRem <asphodel.rem@gmail.com>
2024-02-12 00:37:54 +0700

    Create an interface for math operation

---
+++ lib/core/math/math_operation.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <utils/device_smart_pointer.hpp>
+
+template <typename T>
+class IOperation
+{
+public:
+    IOperation() = default;
+    virtual ~IOperation() = default;
+
+    const SharedPtrGPU<T>& GetResult() const
+    {
+        return result;
+    }
+
+    SharedPtrGPU<T> result;
+};
